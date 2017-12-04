fe9f55008704dc2787e20fdca975c15a1f43856c
Bill Tarr <betarr13@gmail.com>
2017-12-04 17:00:34 -0500

    remove unused import

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const {app, BrowserWindow, globalShortcut} = require('electron');
 const path = require('path');
 const url = require('url');
-const $ = require('jquery');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
