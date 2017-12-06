4a0b4ecc024e2bd747801683c5431ee852763d46
Bill Tarr <betarr13@gmail.com>
2017-12-06 00:30:17 +0000

    do not auto open dev tools

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindow () {
     slashes: true
   }));
 
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
